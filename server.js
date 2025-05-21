40eeb3b43822b22b5392fe71f3365a8ecbf34135
xanderbholden <xanderbholden@gmail.com>
2025-05-20 23:19:41 -0600

    push repositories

--- server.js
+++ server.js
@@ -1,27 +1,42 @@
 /* ******************************************
- * This server.js file is the primary file of the 
+ * This server.js file is the primary file of the
  * application. It is used to control the project.
  *******************************************/
 /* ***********************
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
-const app = express()
 const static = require("./routes/static")
 
+ 
+const app = express()
+ 
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at view root
+ 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
-
+//Index route
+app.get("/", baseController.buildHome)
+ 
+// Inventory routes - Unit 3, Build Inventory route activity
+app.use("/inv", inventoryRoute)
+ 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
 const port = process.env.PORT
 const host = process.env.HOST
-
+ 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
